a1ccbc4572a7e7c9aae12e0db0056a214e7a774b
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-03-02 03:02:55 +0300

    redux kurulumu tamamlandi

--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { legacy_createStore as createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from "react-router-dom";
+import reducer from './reducers/listReducer';
 
-import { BrowserRouter } from "react-router-dom"
-
+const store = createStore(reducer);
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>
 );
 
---
+++ src/reducers/listReducer.js
@@ -0,0 +1,7 @@
+
+
+const reducer = (state, action) => {
+
+}
+
+export default reducer;
